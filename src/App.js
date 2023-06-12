0879126b9f4dac8db92e17a8d9a5cbf3fe203694
Mohammed Abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-06-13 01:08:37 +0300

    add ColorHelper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./ColorHelprs";
 
 function App() {
+  console.log(generatePalette(seedColors[4]));
   return (
     <div>
       <Palette {...seedColors[5]} />
