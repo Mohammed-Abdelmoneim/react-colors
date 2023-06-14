02cf3299a0e44f635f1e80d79c280b538ee575dd
Mohammed Abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-06-14 22:54:02 +0300

    add routes and make them work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import { Component } from "react";
 import Palette from "./Palette";
+import PaletteList from "./PaletteList";
 import seedColors from "./seedColors";
+import { Route, Switch } from "react-router-dom";
 import { generatePalette } from "./ColorHelprs";
 import { random } from "chroma-js";
+import Pallette from "./Palette";
+import { PlaylistAdd } from "@material-ui/icons";
 
-function App() {
-  return (
-    <div>
-      <Palette palette={generatePalette(seedColors[1])} />
-    </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <PaletteList palettes={seedColors} />}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
+      /* <div>
+        <Palette palette={generatePalette(seedColors[1])} />
+      </div> */
+    );
+  }
 }
 
 export default App;
